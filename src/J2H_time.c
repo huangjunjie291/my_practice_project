6009a04cc67398519b234a7499d64b0c0b8d1152
HuanG <huangjunjie291@126.com>
2020-07-26 21:17:44 +0800

    J2Htime add a new func

--- src/J2H_time.c
+++ src/J2H_time.c
@@ -6,3 +6,9 @@ void PrintNowTimeGMT()
     time_t now = time(NULL);
     printf("NOW GTM: %s\n",ctime(&now));
 }
+
+void PrintNowTimeLocal()
+{
+    time_t now = time(NULL);
+    printf("NOW Local Time: %s\n",ctime(&now));
+}
