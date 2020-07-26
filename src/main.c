d120d97c01dfdca3aec5435349688fe03ae8d2de
HuanG <huangjunjie291@126.com>
2020-07-26 11:16:25 +0800

    sprintf

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ int main(int argc,char** argv)
     if(fork()>0) exit(0); //run in back
     int logFilefd;
     char* logFilePath;
-    ssprintf(logFilePath,"%s/log/logFile",env("HOME"));
+    sprintf(logFilePath,"%s/log/logFile",env("HOME"));
     logFilefd = open(logFilePath,O_RDWR|O_APPEND|O_CREATE);
     while(1){
         sleep(1);
