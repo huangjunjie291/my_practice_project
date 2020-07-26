64f6b34829457e593c42cb6772ea146d8868051d
HuanG <huangjunjie291@126.com>
2020-07-26 11:02:50 +0800

    add sleep

--- src/main.c
+++ src/main.c
@@ -10,12 +10,11 @@ int main(int argc,char** argv)
     if(fork()>0) exit(0); //run in back
     int logFilefd;
     char* logFilePath;
-    sprintf(logFilePath,"%s/log/logFile",getenv("HOME"));
-    logFilefd = open(logFilePath,O_RDWR|O_APPEND|O_CREAT);
+    ssprintf(logFilePath,"%s/log/logFile",env("HOME"));
+    logFilefd = open(logFilePath,O_RDWR|O_APPEND|O_CREATE);
     while(1){
         sleep(1);
         PrintNowTimeGMT();
-	sleep(1);
     }
     return 0;
 }
